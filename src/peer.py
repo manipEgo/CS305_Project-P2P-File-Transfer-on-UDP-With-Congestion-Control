03fa1257887c089dee920d9cb7599c4e1678e4a5
YN <1109225291@qq.com>
2022-12-31 14:46:55 +0800

    send_data:bug fix

--- src/peer.py
+++ src/peer.py
@@ -100,18 +100,17 @@ class Peer:
 
     def send_data(self):
         cnt = 0
-        while len(self.send_seq_list) > 0:
+        while len(self.send_seq_list) > 0 and cnt <= self.cwnd:
             if len(self.send_time_dict) == 0:
                 cnt = 0
-            while cnt <= self.cwnd:
-                seq = self.send_seq_list.pop()
-                left = (seq - 1) * MAX_PAYLOAD
-                right = min(seq * MAX_PAYLOAD, CHUNK_SIZE)
-                if 0 < CONFIG.verbose: lprint(f"Sent data[{left}:{right}] to {self}")
-                self.send(DATA,
+            seq = self.send_seq_list.pop()
+            left = (seq - 1) * MAX_PAYLOAD
+            right = min(seq * MAX_PAYLOAD, CHUNK_SIZE)
+            if 0 < CONFIG.verbose: lprint(f"Sent data[{left}:{right}] to {self}")
+            self.send(DATA,
                           seq=seq,
                           data=self.send_chunk[left:right])
-                cnt += 1
+            cnt += 1
         # TODO: Congestion Control
 
     def receive_data(self, data: bytes, seq):
